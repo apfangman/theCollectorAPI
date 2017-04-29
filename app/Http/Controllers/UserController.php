154c50a8252851c96d1334c799c64e1de16a781c
Austin Fangman <austinf@TheCollectorDroplet.localdomain>
2017-04-29 05:40:11 +0000

    Added password checking

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\User as User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
     //Checks user login
     public function checkLogin($aUserEmail, $aUserPassword)
     {
-        $lUser = User::getUserByEmail($aUserEmail);
-
-        if($lUser->password === $aUserPassword)
+	Log::debug(Hash::make($aUserPassword));
+        if(Auth::attempt(['email' => $aUserEmail, 'password' => $aUserPassword]))
         {
-            return $lUser->toJson();
+            return User::getUserByEmail($aUserEmail)->toJson();
         }
 
         return "";
